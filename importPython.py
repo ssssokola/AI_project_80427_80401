53c8c120b3d9559e1c2e0ec9b4566421507cc499
Martin Ivanov <whiteshadow.code@gmail.com>
2013-01-25 22:36:31 +0200

    Little improvements
    Enums and 1 sample sentence

--- importPython.py
+++ importPython.py
@@ -1,9 +1,13 @@
 DB_NAME = 'words_dict.db'
 
-def create_db(name=DB_NAME):
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+def create_db():
 	import sqlite3
 
-	conn = sqlite3.connect(name)
+	conn = sqlite3.connect(DB_NAME)
 	c = conn.cursor()
 	f = c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='words'")
 	if not f.fetchone():
@@ -11,23 +15,29 @@ def create_db(name=DB_NAME):
 	             (id Integer PRIMARY KEY ASC, word text, value Integer, specification Integer, UNIQUE (word) ON CONFLICT REPLACE )''')
 	conn.commit()
 	conn.close()
-	return name
 
-def insert_text_file_in_db(spec, file_name, db_name, regexString):
+
+def insert_text_file_in_db(spec, fileName, regexString):
 	import re
 	import sqlite3
 
-	conn = sqlite3.connect(db_name)
+	conn = sqlite3.connect(DB_NAME)
 	conn.text_factory = str
 	cursor = conn.cursor()
 
-	textFile = open(file_name, 'r')
+	textFile = open(fileName, 'r')
 	for line in textFile:
 		regex_matcher = re.search(regexString, line)
 		if regex_matcher:
 			word = regex_matcher.group(1)
-			value = regex_matcher.group(2)
-			
+
+			if spec == FileTypes.Negating:
+				value = 0
+			elif spec == FileTypes.NonEmotions:
+				value = 0
+			else:
+				value = regex_matcher.group(2)
+
 			query = """INSERT INTO words (word, value, specification) VALUES (?, ?, ?)"""
 			cursor.execute(query, (str(word), value, spec))
 	textFile.close()
@@ -44,7 +54,6 @@ def resolveWord(word, options):
 
 def resolveSentence(sentence):
 	words = sentence.split()
-	i = 0
 	result = []
 	for word in words:
 		value = resolveWord(word, 0)
@@ -53,12 +62,14 @@ def resolveSentence(sentence):
 	for value in result:
 		print value
 
-dbName = create_db()
-insert_text_file_in_db(1, "EmotionLookupTable.txt", dbName, '^(\w*)\*?\s*?(\d|\-\d)')
-insert_text_file_in_db(2, "EmotionLookupTableEXPRESIONS.txt", dbName, '^(\S*)\s*?(\d|\-\d)')
+create_db()
+
+FileTypes = enum('Emotions', 'Emoteicons', 'NonEmotions', 'Negating')
+insert_text_file_in_db(FileTypes.Emotions, "EmotionLookupTable.txt", '^(\w*)\*?\s*?(\d|\-\d)')
+insert_text_file_in_db(FileTypes.Emoteicons, "EmotionLookupTableEXPRESIONS.txt", '^(\S*)\s*?(\d|\-\d)')
+insert_text_file_in_db(FileTypes.NonEmotions, "EnglishWordList.txt", '^(\w*)')
+insert_text_file_in_db(FileTypes.Negating, "NegatingWordList.txt", '^(\w*)')
 
 resolveSentence("This is the last fuckin day at work alol :) ")
-
-# Fix regex for emotions
 
 

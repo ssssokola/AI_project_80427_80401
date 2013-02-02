984eaa8cc1f85df272c6cf23b6695999135fae4c
Martin Ivanov <whiteshadow.code@gmail.com>
2013-02-02 19:49:01 +0200

    Adding app

--- importPython.py
+++ importPython.py
@@ -1,4 +1,5 @@
 DB_NAME = 'words_dict.db'
+LINES = 5
 
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
@@ -35,6 +36,8 @@ def insert_text_file_in_db(spec, fileName, regexString):
 				value = 0
 			elif spec == FileTypes.NonEmotions:
 				value = 0
+			elif spec == FileTypes.EnglishWord:
+				value = 0
 			else:
 				value = regex_matcher.group(2)
 
@@ -55,34 +58,79 @@ def resolveWord(word, options):
 def resolveSentence(sentence):
 
 	words = sentence.split()
-	estimated = []
-	result = []
 
-	lastNegative = False
-	finalEstimation = 0
+	positive = 0
+	negative = 0
 
 	for word in words:
-		valueAndSpec = resolveWord(word, 0)
-		if valueAndSpec:
-			if valueAndSpec['spec'] == FileTypes.Negating:
-				lastNegative = True
-			elif valueAndSpec['spec'] == FileTypes.Emotions:
-				if lastNegative:
-					finalEstimation += valueAndSpec['value'] * -1
-					lastNegative = False
-				else:
-					finalEstimation += valueAndSpec['value']
-			elif valueAndSpec['spec'] == FileTypes.Emoteicons:
-				finalEstimation += valueAndSpec['value']
-	print finalEstimation
+		lowWord = word.lower()
+		valueAndSpec = resolveWord(lowWord, 0)
+		if not valueAndSpec:
+			res = get_twitter_result(word)
+			positive += res[1]
+			negative -= res[2]
+			print word + " " + str(positive) + " " + str(negative)
+		else:
+			print word
 
-create_db()
-FileTypes = enum('Emotions', 'Emoteicons', 'NonEmotions', 'Negating')
-#insert_text_file_in_db(FileTypes.Emotions, "EmotionLookupTable.txt", '^(\w*)\*?\s*?(\d|\-\d)')
-#insert_text_file_in_db(FileTypes.Emoteicons, "EmotionLookupTableEXPRESIONS.txt", '^(\S*)\s*?(\d|\-\d)')
-#insert_text_file_in_db(FileTypes.NonEmotions, "EnglishWordList.txt", '^(\w*)')
-#insert_text_file_in_db(FileTypes.Negating, "NegatingWordList.txt", '^(\w*)')
-insert_text_file_in_db(FileTypes.Negating, "IdiomLookupTable.txt", '^(\w*\s*)*(\d|\-d)')
 
-resolveSentence("This is the last fuckin day at work alol :) ")
-resolveSentence("Hey whats up")
+	# lastNegative = False
+	# finalEstimation = 0
+
+	# for word in words:
+	# 	valueAndSpec = resolveWord(word, 0)
+	# 	if valueAndSpec:
+	# 		if valueAndSpec['spec'] == FileTypes.Negating:
+	# 			lastNegative = True
+	# 		elif valueAndSpec['spec'] == FileTypes.Emotions:
+	# 			if lastNegative:
+	# 				finalEstimation += valueAndSpec['value'] * -1
+	# 				lastNegative = False
+	# 			else:
+	# 				finalEstimation += valueAndSpec['value']
+	# 		elif valueAndSpec['spec'] == FileTypes.Emoteicons:
+	# 			finalEstimation += valueAndSpec['value']
+	# print finalEstimation
+
+def twitter_client_results(word):
+	import subprocess
+	output = subprocess.check_output(["java", "-jar", "SentiTwitter.jar", word])
+	found = False
+	i = 1
+	foundLine = 0
+	while not found:
+		# print "DEBUG"
+		if output[-i] == '\n':
+			foundLine+=1
+			# print "DEBUG if 1"
+		if foundLine == LINES:
+			# print "DEBUG if 2"
+			break
+		i+=1
+
+	res = output[len(output)-i:]
+	print res
+	values = [int(i) for i in res.split()]
+	return values
+
+def get_twitter_result(word):
+	results = twitter_client_results(word)
+	positive = results[1]
+	negative = results[2]
+	# if abs(positive) + abs(negative) > 0:
+	# 	return round(1.0 * positive / ( abs(positive) + abs(negative)) * 10 - 5)
+	# else:
+	# 	return 0
+	return results
+
+# create_db()
+FileTypes = enum('Emotions', 'Emoteicons', 'NonEmotions', 'Negating', 'Idiom', 'EnglishWord')
+# insert_text_file_in_db(FileTypes.Emotions, "EmotionLookupTable.txt", '^(\w*)\*?\s*?(\d|\-\d)')
+# insert_text_file_in_db(FileTypes.Emoteicons, "EmotionLookupTableEXPRESIONS.txt", '^(\S*)\s*?(\d|\-\d)')
+# insert_text_file_in_db(FileTypes.NonEmotions, "EnglishWordList.txt", '^(\w*)')
+# insert_text_file_in_db(FileTypes.Negating, "NegatingWordList.txt", '^(\w*)')
+# insert_text_file_in_db(FileTypes.Idiom, "IdiomLookupTable.txt", '^(\w*\s*)*(\d|\-d)')
+# insert_text_file_in_db(FileTypes.EnglishWord, "EnglishWordList.txt", '^(\w*)')
+#resolveSentence("This is the last fuckin day at work alol :) ")
+#resolveSentence("Hey whats up")
+resolveSentence("This will be the most fucking crappy jurney ever!")
